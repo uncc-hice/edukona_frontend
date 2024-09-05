ea9bdb6a62fde0ea89295d14e575745603351fd0
“Aryan <aaladar@uncc.edu>
2024-09-04 23:59:43 -0400

    Added navigation to recordings page and updated the way the existing buttons at the bottom look

--- src/pages/InstructorDashboard.jsx
+++ src/pages/InstructorDashboard.jsx
@@ -4,6 +4,10 @@ import QuizList from "../blocks/QuizList";
 import Button from '@mui/material/Button'
 import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
+import Box from '@mui/material/Box';
+import QuizIcon from '@mui/icons-material/Quiz';
+import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
+import RecordVoiceOverIcon from '@mui/icons-material/RecordVoiceOver';
 
 const InstructorDashBoard = () => {
 
@@ -12,21 +16,30 @@ const InstructorDashBoard = () => {
   const handleCreateQuiz = () => {
     navigate('/create-quiz')
   };
+
+  const handleNavigateToRecordings = () => {
+    navigate('/recordings')
+  };
+
   return (<div>
     <Navbar/>
     <h1 style={{ textAlign: 'Left', margin: '20px' }}>Your Quizzes</h1>
     <QuizList/>
-    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+    <Box sx={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', marginTop: '20px' }}>
       <Link to="/your-sessions">
-        <Button variant="contained" color="primary" size="large" style={{ width: '80%', maxWidth: '1000px', marginBottom: '10px' }}>
+        <Button startIcon={<MeetingRoomIcon />} variant="outlined" color="primary" size="large" style={{ marginBottom: '10px' }}>
           Your Sessions
         </Button>
       </Link>
-      <Button variant="contained" color="primary" size="large" style={{ width: '80%', maxWidth: '1000px' }}
+      <Button startIcon={<QuizIcon />} variant="outlined" color="primary" size="large" style={{ marginBottom: '10px' }}
               onClick={handleCreateQuiz}>
         Create New Quiz
       </Button>
-    </div>
+      <Button startIcon={<RecordVoiceOverIcon />} variant="outlined" color="primary" size="large"
+              onClick={handleNavigateToRecordings}>
+        Go to Recordings
+      </Button>
+    </Box>
   </div>);
 };
 
