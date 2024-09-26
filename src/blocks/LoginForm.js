a0556bf18217c37edb452462ff600677a300445f
amhajja <amhajja@gmail.com>
2024-09-26 19:17:08 -0400

    Integrate Google OAuth in LoginForm
    Added Google OAuth login functionality to LoginForm.js, enabling users to sign in using their Google accounts. Included necessary axios requests for backend authentication and updated UI to handle different login states.

--- src/blocks/LoginForm.js
+++ src/blocks/LoginForm.js
@@ -1,91 +1,128 @@
-import {useContext, useState} from "react";
+import { useContext, useState, useEffect } from "react";
 import { TextField, Button, Typography, Container, Grid } from '@mui/material';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
-import {UserContext} from "../UserContext";
-
+import { UserContext } from "../UserContext";
+import { Box } from "@mui/system";
+import axios from "axios";
 
 const LoginForm = ({ toggleForm }) => {
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
-	const [error, setError] = useState('');
-	const navigate = useNavigate();
-	const {login} = useContext(UserContext);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+  const { user, login, setIsLoggedIn, setToken } = useContext(UserContext);
 
-	const styles = {
-		error: {
-			color: "red",
-			fontWeight: "bold",
-			textAlign: "center",
-		}
-	}
+  useEffect(() => {
+    if (user) {
+      navigate('/dashboard');
+    }
+  }, [user, navigate]);
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		login(username, password, navigate, setError);
-	};
+  const styles = {
+    error: {
+      color: "red", fontWeight: "bold", textAlign: "center",
+    }
+  }
 
-	return (
-		<Container component="main" maxWidth="xs" style={{ height: '100vh' }}>
-			<Grid
-				container
-				direction="column"
-				justifyContent="center" // This centers the form vertically
-				alignItems="center"    // This centers the form horizontally
-				style={{ minHeight: '100vh' }}
-			>
-				<div>
-					<Typography variant="h4" component="h1" align="center">
-						Login
-					</Typography>
-					<form onSubmit={handleSubmit}>
-						<TextField
-							margin="normal"
-							required
-							fullWidth
-							id="username"
-							label="Username"
-							name="username"
-							autoComplete="username"
-							autoFocus
-							value={username}
-							onChange={(e) => setUsername(e.target.value)}
-						/>
-						<TextField
-							margin="normal"
-							required
-							fullWidth
-							name="password"
-							label="Password"
-							type="password"
-							id="password"
-							autoComplete="current-password"
-							value={password}
-							onChange={(e) => setPassword(e.target.value)}
-						/>
-						{
-							error !== '' && <p style={styles.error}>{error}</p>
-						}
-						<Button
-							type="submit"
-							fullWidth
-							variant="contained"
-							color="primary"
-							sx={{ mt: 3, mb: 2 }}
-						>
-							Login
-						</Button>
-						<Grid container justifyContent="center">
-							<Grid item>
-								<Button onClick={toggleForm} sx={{ mt: 3, mb: 2 }}>
-									Don't have an account? Sign Up
-								</Button>
-							</Grid>
-						</Grid>
-					</form>
-				</div>
-			</Grid>
-		</Container>
-	);
+  const handleGoogleSuccess = async (response) => {
+    const axiosUrl = 'https://api.edukona.com/auth/google/'; // Update to your actual backend endpoint
+    console.log('Google Login success:', response);
+    const { credential } = response;
+    try {
+      // Send the Google ID token to your backend for verification and token issuance
+      const res = await axios.post(axiosUrl, { token: credential });
+
+      localStorage.setItem('token', res.data.token);
+      localStorage.setItem('user', res.data.user);
+
+      setIsLoggedIn(true);
+      setToken(res.data.token);
+
+      navigate('/dashboard');
+
+    } catch (error) {
+      console.error('Google Login error:', error);
+      setError('Google Login failed. Please try again.');
+    }
+  };
+
+  // Handle Google Sign-In failure
+  const handleGoogleError = () => {
+    setError('Google Login failed. Please try again.');
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    login(username, password, setError, navigate);
+  };
+
+
+  return (<Container component="main" maxWidth="xs" style={{ height: '100vh' }}>
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      style={{ minHeight: '100vh' }}
+    >
+      <div>
+        <Typography variant="h4" component="h1" align="center">
+          Login
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {error !== '' && <p style={styles.error}>{error}</p>}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Login
+          </Button>
+          <Grid container justifyContent="center">
+            <Grid item>
+              <Button onClick={toggleForm} sx={{ mt: 3, mb: 2 }}>
+                Don't have an account? Sign Up
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+        {/* Google Login Button */}
+        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+          <Box display="flex" justifyContent="center">
+            <GoogleLogin onSuccess={handleGoogleSuccess} onError={handleGoogleError}/>
+          </Box>
+        </GoogleOAuthProvider>
+      </div>
+    </Grid>
+  </Container>);
 };
 
 export default LoginForm;
