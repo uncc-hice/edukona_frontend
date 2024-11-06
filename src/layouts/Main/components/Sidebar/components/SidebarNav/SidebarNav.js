755cb55bd23cf626362122b66bb3deba18905f20
BruhLemma-Yadecha <98387741+BruhLemma-Yadecha@users.noreply.github.com>
2024-11-05 23:24:00 -0500

    feat: update SidebarNav to conditionally render buttons based on user login status

--- src/layouts/Main/components/Sidebar/components/SidebarNav/SidebarNav.js
+++ src/layouts/Main/components/Sidebar/components/SidebarNav/SidebarNav.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import { useTheme } from '@mui/material/styles';
+import { UserContext } from '../../../../../../UserContext';
 
 const SidebarNav = ({ pages }) => {
   const theme = useTheme();
   const { mode } = theme.palette;
+  const { isLoggedIn, logout } = useContext(UserContext);
 
   return (
     <Box>
@@ -25,24 +27,56 @@ const SidebarNav = ({ pages }) => {
         </Box>
       </Box>
       <Box paddingX={2} paddingY={2}>
-        <Box marginTop={2}>
-          <Button
-            size={'large'}
-            variant="contained"
-            color="primary"
-            fullWidth
-            component="a"
-            target="blank"
-            href="/signup"
-          >
-            Sign Up
-          </Button>
-        </Box>
-        <Box marginTop={1}>
-          <Button size={'large'} variant="outlined" fullWidth component="a" href="/login">
-            Login
-          </Button>
-        </Box>
+        {!isLoggedIn ? (
+          <>
+            <Box marginTop={2}>
+              <Button
+                size={'large'}
+                variant="contained"
+                color="primary"
+                fullWidth
+                component="a"
+                target="blank"
+                href="/signup"
+              >
+                Sign Up
+              </Button>
+            </Box>
+            <Box marginTop={1}>
+              <Button size={'large'} variant="outlined" fullWidth component="a" href="/login">
+                Login
+              </Button>
+            </Box>
+          </>
+        ) : (
+          <>
+            <Box marginTop={2}>
+              <Button variant="outlined" color="primary" component="a" href="/dashboard" size="large" fullWidth>
+                Quizzes
+              </Button>
+            </Box>
+            <Box marginTop={1}>
+              <Button variant="outlined" color="primary" component="a" href="/recordings" size="large" fullWidth>
+                Recordings
+              </Button>
+            </Box>
+            <Box marginTop={1}>
+              <Button variant="outlined" color="primary" href="/team" size="large" fullWidth>
+                Team
+              </Button>
+            </Box>
+            <Box marginTop={1}>
+              <Button variant="outlined" color="primary" component="a" href="/account-general" size="large" fullWidth>
+                Profile
+              </Button>
+            </Box>
+            <Box marginTop={1}>
+              <Button variant="contained" color="primary" onClick={logout} size="large" fullWidth>
+                Logout
+              </Button>
+            </Box>
+          </>
+        )}
       </Box>
     </Box>
   );
