b3b5948ee90b9a2a236e43680c6a86c68e210c5b
BruhLemma-Yadecha <98387741+BruhLemma-Yadecha@users.noreply.github.com>
2024-11-05 21:32:04 -0500

    fix: add style revisions and edit conditions

--- src/blocks/StudentAnswerOption.jsx
+++ src/blocks/StudentAnswerOption.jsx
@@ -7,28 +7,41 @@ const getColor = (index) => {
   return colors[index % colors.length]; // Cycle through colors based on index
 };
 
-const StyledButtonBase = styled(ButtonBase)(({ theme, index, selected, submitted }) => ({
-  backgroundColor: getColor(index), // Set background color based on index
-  textAlign: 'center',
-  width: '100%',
-  minHeight: '220px',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  borderRadius: theme.shape.borderRadius,
-  '&:hover': {
+const StyledButtonBase = styled(ButtonBase)(({ theme, index, selected, submitted, disabled }) => {
+  const baseStyles = {
+    backgroundColor: getColor(index), // Set background color based on index
+    textAlign: 'center',
+    width: '100%',
+    minHeight: '220px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    borderRadius: theme.shape.borderRadius,
+    '&:hover': {
+      opacity: 0.9,
+      boxShadow: theme.shadows[4],
+    },
+    color: theme.palette.getContrastText(getColor(index)),
+  };
+
+  const disabledStyles = {
+    opacity: 0.3,
+    pointerEvents: 'none',
+  };
+
+  const selectedStyles = {
     opacity: 0.9,
-    boxShadow: theme.shadows[4],
-  },
-  color: theme.palette.getContrastText(getColor(index)),
-  ...(!selected &&
-    submitted && {
-      opacity: 0.3, // Adjust as needed to indicate disabled state visually
-    }),
-  ...(selected && {
     boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.4)',
-  }),
-}));
+  };
+
+  return {
+    ...baseStyles,
+    ...(disabled && !selected && disabledStyles),
+    ...(disabled && selected && { ...disabledStyles, ...selectedStyles }),
+    ...(!disabled && !selected && { opacity: 0.3 }),
+    ...(!disabled && selected && selectedStyles),
+  };
+});
 const StudentAnswerOption = ({ answer, index, onClick, selected, submitted, disabled }) => {
   return (
     <StyledButtonBase
