08edf80658ccab99c894640d145149a495ddc062
“Aryan <aryan.aladar@gmail.com>
2024-11-15 10:48:59 -0500

    Added the menu button to edit title

--- src/blocks/CustomizedMenus.jsx
+++ src/blocks/CustomizedMenus.jsx
@@ -3,6 +3,7 @@ import { styled, alpha } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import EditIcon from '@mui/icons-material/Edit';
 import Divider from '@mui/material/Divider';
 import DeleteIcon from '@mui/icons-material/Delete';
 import QuizIcon from '@mui/icons-material/Quiz';
@@ -26,7 +27,8 @@ const StyledMenu = styled((props) => (
     borderRadius: 6,
     marginTop: theme.spacing(1),
     minWidth: 180,
-    color: theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+    color:
+      theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
     boxShadow:
       'rgb(255, 255, 255) 0px 0px 0px 0px, ' +
       'rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, ' +
@@ -42,14 +44,24 @@ const StyledMenu = styled((props) => (
         marginRight: theme.spacing(1.5),
       },
       '&:active': {
-        backgroundColor: alpha(theme.palette.primary.main, theme.palette.action.selectedOpacity),
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity
+        ),
       },
     },
   },
 }));
 
 export default function CustomizedMenus(props) {
-  const { recording, handleOpenDialogue, setSelectedRecording, setOpenNewRecording } = props;
+  const {
+    recording,
+    handleOpenDialogue,
+    setSelectedRecording,
+    setOpenNewRecording,
+    setOpenEditTitleDialog,
+    setNewTitle
+  } = props;
 
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
@@ -69,6 +81,13 @@ export default function CustomizedMenus(props) {
   const handleCreateAndStartQuiz = (event) => {
     event.stopPropagation();
     setOpenNewRecording(true);
+    handleClose(event);
+  };
+
+  const handleEditTitle = (event) => {
+    event.stopPropagation();
+    setOpenEditTitleDialog(true);
+    setNewTitle(recording.title);
     handleClose(event);
   };
 
@@ -106,6 +125,10 @@ export default function CustomizedMenus(props) {
           Create and Start Quiz
         </MenuItem>
         <Divider sx={{ my: 0.5 }} />
+        <MenuItem onClick={handleEditTitle} disableRipple>
+          <EditIcon />
+          Edit Title
+        </MenuItem>
         <MenuItem onClick={handleDeleteRecording} disableRipple>
           <DeleteIcon />
           Delete Recording
