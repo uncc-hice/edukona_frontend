e0c2763ddbe551d69378854c4b5fd92c4cdc0bd3
austinfhunter <austinfhunter@gmail.com>
2024-11-17 23:49:35 -0500

    add delete functionality to EditableQuestion component

---
+++ src/blocks/DeleteQuestionDialog.jsx
@@ -0,0 +1,44 @@
+import { Delete, Undo } from '@mui/icons-material';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Typography } from '@mui/material';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+const DeleteQuestionDialog = ({ question, token, open, setOpen, onUpdate }) => {
+  const handleDelete = () =>
+    toast.promise(
+      axios
+        .delete(`https://api.edukona.com/question/${question.id}/`, { headers: { Authorization: `Token ${token}` } })
+        .then(() => {
+          setOpen(false);
+          onUpdate();
+        }),
+      {
+        pending: 'Deleting question...',
+        success: 'Successfully deleted question.',
+        error: 'Failed to delete question.',
+      }
+    );
+  return (
+    <Dialog open={open} fullWidth>
+      <DialogTitle>Confirm Question Deletion</DialogTitle>
+      <DialogContent style={{ display: 'flex', flexDirection: 'column' }}>
+        <Typography variant="p">Are you sure you want to delete the question "{question.question_text}"</Typography>
+        <Typography variant="subtitle1" fontWeight={'bold'}>
+          This cannot be undone.
+        </Typography>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => setOpen(false)}>
+          <Undo />
+          Cancel
+        </Button>
+        <Button color="error" onClick={handleDelete}>
+          <Delete />
+          Delete
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default DeleteQuestionDialog;
