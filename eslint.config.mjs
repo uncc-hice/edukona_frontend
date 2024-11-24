b3dedb452ea6943fdb31fde7db68ca02fda65238
BruhLemma-Yadecha <98387741+BruhLemma-Yadecha@users.noreply.github.com>
2024-11-24 16:02:59 -0500

    feat: enhance ESLint configuration with React support and additional rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,51 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import globals from 'globals';
+import reactPlugin from 'eslint-plugin-react';
+
 
 export default tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.recommended,
+  tseslint.configs.base,
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat['jsx-runtime'],
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      globals : {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        }
+    }
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-empty': 'warn',
+      'no-undef': 'warn',
+      'react/no-unescaped-entities': 'warn',
+      'react/jsx-key': 'warn',
+      'react/display-name': 'warn',
+    },
+  },
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    rules: {
+      'react/prop-types': 'off',
+    },
+  },
+  {
+    plugins: {
+      react: reactPlugin,
+    },
+    settings : {
+      react: {
+        version: 'detect',
+      },
+    },
+  }
 );
