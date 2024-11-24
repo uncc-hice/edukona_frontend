6e4e493da7d7deae6f6ee1fb359b3a291e5099ca
BruhLemma-Yadecha <98387741+BruhLemma-Yadecha@users.noreply.github.com>
2024-11-24 17:28:29 -0500

    fix: remove eslint errors

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom/extend-expect';
+import { test, expect } from '@jest/globals';
 
 test('renders learn react link', () => {
   render(<App />);
