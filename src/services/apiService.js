2034cc0706a781ecc1d7834b21f905cffd76b487
BruhLemma-Yadecha <98387741+BruhLemma-Yadecha@users.noreply.github.com>
2024-11-28 17:49:21 -0500

    feat: add fetchQuiz and updateQuiz functions to apiService

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -76,3 +76,6 @@ export const fetchProfile = () =>
       'Content-Type': 'application/json',
     },
   });
+
+export const fetchQuiz = (id) => api.get(`quiz/${id}`);
+export const updateQuiz = (id, data) => api.put(`quiz/${id}`, data);
