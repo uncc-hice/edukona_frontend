17c3f5d6e8980f33484544769ff6dadfdb33a1bf
BruhLemma-Yadecha <98387741+BruhLemma-Yadecha@users.noreply.github.com>
2025-01-19 19:03:41 -0500

    refactor: streamline token management and improve code readability in lambdaService

--- src/services/lambdaService.js
+++ src/services/lambdaService.js
@@ -1,27 +1,56 @@
 import axios from 'axios';
+import { refreshAccessToken } from './apiService';
 
 const token = localStorage.getItem('token');
+const jwtAccessToken = localStorage.getItem('accessToken');
 
-export const createQuiz = (quizSettings, recordingId) =>
+const getAuthHeader = () => {
+  if (jwtAccessToken !== null) {
+    return `Bearer ${jwtAccessToken}`;
+  } else if (token !== null) {
+    return `Token ${token}`;
+  } else {
+    return '';
+  }
+};
+
+const topupTokenAndRun = (promise) => {
+  const refreshToken = localStorage.getItem('refreshToken');
+  return refreshAccessToken(refreshToken)
+    .then(() => {
+      console.log('Token refreshed');
+      return promise;
+    })
+    .catch((err) => {
+      console.error(`Could not refresh token: ${err}`);
+      return;
+    });
+};
+
+const createQuizPromise = (quizSettings, recordingId) =>
   axios.post(
     'https://jtsw0t0x32.execute-api.us-west-2.amazonaws.com/Prod/create_quiz_from_transcript',
     { ...quizSettings, recording_id: recordingId },
     {
       headers: {
-        Authorization: `Token ${token}`,
+        Authorization: getAuthHeader(),
         'Content-Type': 'application/json',
       },
     }
   );
 
-export const generateSummary = (recordingId) =>
+export const createQuiz = (quizSettings, recordingId) => topupTokenAndRun(createQuizPromise(quizSettings, recordingId));
+
+export const generateSummaryPromise = (recordingId) =>
   axios.post(
     'https://6y2dyfv9k1.execute-api.us-west-2.amazonaws.com/Prod/create_summary_from_transcript',
     { recording_id: recordingId },
     {
       headers: {
-        Authorization: `Token ${token}`,
+        Authorization: getAuthHeader(),
         'Content-Type': 'application/json',
       },
     }
   );
+
+export const generateSummary = (recordingId) => topupTokenAndRun(generateSummaryPromise(recordingId));
