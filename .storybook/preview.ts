fc009a402f3d89aa3da20af0dbaf910e2c9dc8a8
thepillar.ai@gmail.com <amhajja@gmail.com>
2025-01-25 08:58:38 -0500

    Add initial Storybook setup files and assets
    This commit establishes the foundational setup for Storybook, including core components like `Button`, associated styles, configuration files, and assets. It also introduces MDX content for project configuration and design documentation.

---
+++ .storybook/preview.ts
@@ -0,0 +1,23 @@
+import type { Preview } from '@storybook/react';
+import { initialize, mswLoader } from 'msw-storybook-addon';
+
+/*
+ * Initializes MSW
+ * See https://github.com/mswjs/msw-storybook-addon#configuring-msw
+ * to learn how to customize it
+ */
+initialize();
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  loaders: [mswLoader], // 👈 Add the MSW loader to all stories
+};
+
+export default preview;
