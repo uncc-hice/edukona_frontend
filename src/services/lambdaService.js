6cc408e7bcb0e81501575d285636c48e37ee0dd6
BruhLemma-Yadecha <98387741+BruhLemma-Yadecha@users.noreply.github.com>
2025-04-22 12:05:19 -0400

    refactor: simplify quiz and summary creation by using callWithFreshToken

--- src/services/lambdaService.js
+++ src/services/lambdaService.js
@@ -1,50 +1,21 @@
-import axios from 'axios';
-import { refreshAccessToken } from './apiService';
+import { api, callWithFreshToken } from './apiService';
 
-const accessToken = localStorage.getItem('accessToken');
+const LAMBDA_TIMEOUT = 30_000;
 
-const getAuthHeader = () => {
-  if (accessToken !== null) return `Bearer ${accessToken}`;
-  return '';
-};
-
-const topupTokenAndRun = (promise) => {
-  const refreshToken = localStorage.getItem('refreshToken');
-  return refreshAccessToken(refreshToken)
-    .then(() => {
-      console.log('Token refreshed');
-      return promise;
-    })
-    .catch((err) => {
-      console.error(`Could not refresh token: ${err}`);
-      return;
-    });
-};
-
-const createQuizPromise = (quizSettings, recordingId) =>
-  axios.post(
-    'https://jtsw0t0x32.execute-api.us-west-2.amazonaws.com/Prod/create_quiz_from_transcript',
-    { ...quizSettings, recording_id: recordingId },
-    {
-      headers: {
-        Authorization: getAuthHeader(),
-        'Content-Type': 'application/json',
-      },
-    }
+export const createQuiz = (quizSettings, recordingId) =>
+  callWithFreshToken(() =>
+    api.post(
+      'https://jtsw0t0x32.execute-api.us-west-2.amazonaws.com/Prod/create_quiz_from_transcript',
+      { ...quizSettings, recording_id: recordingId },
+      { timeout: LAMBDA_TIMEOUT }
+    )
   );
 
-export const createQuiz = (quizSettings, recordingId) => topupTokenAndRun(createQuizPromise(quizSettings, recordingId));
-
-export const generateSummaryPromise = (recordingId) =>
-  axios.post(
-    'https://6y2dyfv9k1.execute-api.us-west-2.amazonaws.com/Prod/create_summary_from_transcript',
-    { recording_id: recordingId },
-    {
-      headers: {
-        Authorization: getAuthHeader(),
-        'Content-Type': 'application/json',
-      },
-    }
+export const generateSummary = (recordingId) =>
+  callWithFreshToken(() =>
+    api.post(
+      'https://6y2dyfv9k1.execute-api.us-west-2.amazonaws.com/Prod/create_summary_from_transcript',
+      { recording_id: recordingId },
+      { timeout: LAMBDA_TIMEOUT }
+    )
   );
-
-export const generateSummary = (recordingId) => topupTokenAndRun(generateSummaryPromise(recordingId));
